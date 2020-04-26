63bc9015c753ff75aa2f7a8b5eb6b51462874f6c
Jerel Henderson <henderson.jerel@yahoo.com>
2020-04-26 02:58:49 +0000

    Add changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,10 +7,12 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views, states
 from flask_cors import CORS
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={r"\*": {"origins": ['0.0.0.0']})
+app.url_map.strict_slashes=False
+CORS(app, resources="\*", origins='0.0.0.0')
 
 @app.teardown_appcontext
 def close_db(Exception):
@@ -25,4 +27,6 @@ def page_not_found(e):
 
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', port=5000)
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=int(5000))
