e331878e872cc6037e9e57e08532fd3a28205b73
Jerel Henderson <henderson.jerel@yahoo.com>
2020-04-26 02:45:10 +0000

    +x files

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,7 +6,7 @@ appy.py - handles Flask app related functions
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views, states
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
